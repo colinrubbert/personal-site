e75b352e4fbeb570e56aebe21e2edb4a5d0c749b
Colin Rubbert <colinrubbert@gmail.com>
2015-11-18 19:32:17 -0600

    add js onclick to show and hide gallery categories

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,6 +1,7 @@
  $(function() {
   mobileNav();
   fancyBox();
+  gallerySelection();
 });
 
 // Function to open and close navigation on movile devices
@@ -30,5 +31,34 @@ function fancyBox() {
         height: 50
       }
     }
+  });
+}
+
+// Function for gallerySelection implementation
+function gallerySelection() {
+  $('.people-category').ready(function() {
+    $('.people-category').addClass('active');
+    $('.people').addClass('photography-grid');
+  });
+
+  $('.people-category').click(function() {
+    $('.people-category').addClass('active');
+    $('.places-category, .things-category').removeClass('active');
+    $('.places, .things').removeClass('photography-grid');
+    $('.people').addClass('photography-grid');
+  });
+
+  $('.places-category').click(function() {
+    $('.places-category').addClass('active');
+    $('.people-category, .things-category').removeClass('active');
+    $('.people, .things').removeClass('photography-grid');
+    $('.places').addClass('photography-grid');
+  });
+
+  $('.things-category').click(function() {
+    $('.things-category').addClass('active');
+    $('.people-category, .places-category').removeClass('active');
+    $('.people, .places').removeClass('photography-grid');
+    $('.things').addClass('photography-grid');
   });
 }
